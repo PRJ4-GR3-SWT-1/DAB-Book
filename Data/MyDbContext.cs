469f579fa88e97df0e81540505b7bb323ef3d589
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-03-10 11:30:51 +0100

    flere nuget

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Extensions;
-
+//using Microsoft.EntityFrameworkCore.Extensions;
 using EFModels.Models;
 
 
